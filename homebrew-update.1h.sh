8603c49b511232ed1e3639a6b79944be07cff47b
Nils Stein <social.nst@mailbox.org>
2023-04-28 08:21:41 +0200

    working

--- homebrew-update.1h.sh
+++ homebrew-update.1h.sh
@@ -3,11 +3,20 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SCRIPT_NAME=$(basename "$0")
 
-formulae=$(brew install --dry-run --formulae --quiet $(brew list --formulae) 2>/dev/null | sed -e '1,/==> Would install/d')
-casks=$(brew install --dry-run --casks --quiet $(brew list --casks) 2>/dev/null | sed -e '1,/==> Would install/d')
+brew (){
+    /opt/homebrew/bin/brew $@
+}
 
-count_formulae=$(echo $formulae | xargs | wc -l | xargs)
-count_casks=$(echo $casks | xargs | wc -l | xargs)
+brew update 2>&1 >/dev/null
+
+formulae=$(brew install --dry-run --formulae --quiet $(brew list --formulae) 2>/dev/null | sed -e '1,/==> Would install/d' | tr " " "\n")
+casks=$(brew install --dry-run --casks --quiet $(brew list --casks) 2>/dev/null | sed -e '1,/==> Would install/d' | tr " " "\n")
+
+count_formulae=0
+count_casks=0
+
+count_formulae=$(echo $formulae | wc -w | xargs)
+count_casks=$(echo $cask | wc -w | xargs)
 
 count_all=$((count_formulae + count_casks))
 
@@ -17,28 +26,18 @@ if [ $# -eq 0 ]; then
     echo " ${count_all} | trim=false | templateImage=${icon}"
     echo "---"
     if [[ "${count_formulae}" != "0" ]]; then
-        if [[ "${count_formulae}" == "1" ]]; then
-            echo "$count_formulae formula to update"
-        else
-            echo "$count_formulae formulae to update"
-        fi
-            for line in $formulae; do 
-                echo "$line" | grep "[a-z]" | sed 's_\(.*\)_& | bash=brew param1=install param2=--formula param3=& param4=\&\& param5=exit terminal=true refresh=true_g'
-            done
+        for line in $formulae; do 
+            echo "$line" | grep "[a-z]" | sed 's_\(.*\)_& | bash=brew param1=install param2=--formula param3=& param4=\&\& param5=exit terminal=true refresh=true_g'
+        done
     fi
     if [[ "${count_formulae}" == "0" ]]; then
         echo "No formulae to update"
     fi
     echo "---"
     if [[ "${count_casks}" != "0" ]]; then
-        if [[ "${count_casks}" == "1" ]]; then
-            echo "$count_casks cask to update"
-        else
-            echo "$count_casks casks to update"
-        fi
-            for line in $casks; do 
-                echo "$line" | grep "[a-z]" | sed 's_\(.*\)_& | bash=brew param1=install param2=--cask param3=& param4=\&\& param5=exit terminal=true refresh=true_g'
-            done
+        for line in $casks; do 
+            echo "$line" | grep "[a-z]" | sed 's_\(.*\)_& | bash=brew param1=install param2=--cask param3=& param4=\&\& param5=exit terminal=true refresh=true_g'
+        done
     fi
     if [[ "${count_casks}" == "0" ]]; then
         echo "No casks to update"
@@ -50,13 +49,13 @@ if [ $# -eq 0 ]; then
     echo "Refresh | refresh=true"
 else
     if [[ $1 == 'upgrade' ]]; then
-        brew update
         if [[ "${count_formulae}" != "0" ]]; then
             brew upgrade --formulae ${formulae}
         fi
         if [[ "${count_casks}" != "0" ]]; then
             brew upgrade --casks ${casks}
         fi
+        /usr/bin/open --background xbar://app.xbarapp.com/refreshPlugin?path=${SCRIPT_NAME}
     fi
     if [[ $1 == 'cleanup' ]]; then
         brew cleanup --formulae
