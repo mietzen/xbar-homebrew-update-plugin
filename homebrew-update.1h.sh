13f0993de7d0d4cf840d82d31c3f52df74730c08
Nils Stein <31704359+mietzen@users.noreply.github.com>
2024-09-09 08:26:23 +0200

    better home brew handling

--- homebrew-update.1h.sh
+++ homebrew-update.1h.sh
@@ -7,10 +7,10 @@ ASSETS_DIR=${SCRIPT_DIR}/homebrew-update/assets
 export HOMEBREW_CASK_OPTS=--no-quarantine
 
 if [ -f /opt/homebrew/bin/brew ]; then
-    HOMEBREW_BIN=/opt/homebrew/bin/brew
+    HOMEBREW_BIN=/opt/homebrew/bin
 else
     if [ -f /usr/local/bin/brew ]; then
-        HOMEBREW_BIN=/usr/local/bin/brew 
+        HOMEBREW_BIN=/usr/local/bin
     else
         echo "Failed!"
         echo "---"
@@ -19,38 +19,42 @@ else
     fi
 fi
 
-brew (){
-    ${HOMEBREW_BIN} $@
-}
+PATH="${HOMEBREW_BIN}:${PATH}"
 
 brew update >> /dev/null 2>&1
 
-formulae=$(brew upgrade --dry-run --formulae --quiet $(brew list --formulae) 2>/dev/null | grep -e "^[a-z].*" | cut -d' ' -f1 | sort -n | uniq)
-casks=$(brew upgrade --dry-run --casks --quiet $(brew list --casks) 2>/dev/null | grep -e "^[a-z].*" | cut -d' ' -f1 | sort -n | uniq)
+outdated="$(brew outdated --greedy-auto-updates --json)"
+
+formulae=$(echo "${outdated}" | jq '.formulae.[].name' -r | xargs)
+casks=$(echo "${outdated}" | jq '.casks.[].name' -r | xargs)
 
 count_formulae=$(echo ${formulae} | wc -w | xargs)
 count_casks=$(echo ${casks} | wc -w | xargs)
 
 count_all=$((count_formulae + count_casks))
 
-if [ -f ${ASSETS_DIR}/.darkmode ]; then
-    icon=$(base64 -i ${ASSETS_DIR}/icon_dark.png)
-    icon_attention=$(base64 -i ${ASSETS_DIR}/icon_attention_dark.png)
-else
-    icon=$(base64 -i ${ASSETS_DIR}/icon.png)
-    icon_attention=$(base64 -i ${ASSETS_DIR}/icon_attention.png)
-fi
+icon=$(base64 -i "${ASSETS_DIR}/icon.png")
+icon_attention=$(base64 -i "${ASSETS_DIR}/icon_attention.png")
 
 if [ $# -eq 0 ]; then
     if [[ "${count_all}" != "0" ]]; then
         echo " | templateImage=${icon_attention}"
-        if [ -f ${ASSETS_DIR}/.notify ]; then
-            ${ASSETS_DIR}/notifier.app/Contents/MacOS/applet
-        fi
     else
         echo " | templateImage=${icon}"
     fi
     echo "---"
+    if [[ -f ${ASSETS_DIR}/.errors ]]; then
+        echo "Errors while upgrading: | color=red"
+        errors=$(cat ${ASSETS_DIR}/.errors)
+        for err_pkg in ${errors}; do
+            echo "- ${err_pkg}"
+            echo "--Show log | bash=less param1=${ASSETS_DIR}/.log terminal=true refresh=true"
+            echo "--Reinstall | bash=${SCRIPT_DIR}/${SCRIPT_NAME} param1=reinstall param2=${err_pkg} terminal=true refresh=true"
+            echo "--Uninstall | bash=${SCRIPT_DIR}/${SCRIPT_NAME} param1=uninstall param2=${err_pkg} terminal=true refresh=true"
+        done
+        echo "Clear Errors | color=#68696C | bash=rm param1=-rf param2=${ASSETS_DIR}/.errors terminal=false refresh=true"
+        echo "---"
+    fi
     if [[ "${count_all}" != "0" ]]; then
         if [[ "${count_formulae}" != "0" ]]; then
             ident=''
@@ -59,7 +63,7 @@ if [ $# -eq 0 ]; then
                 echo "${count_formulae} Formulae can be update"
             fi
             for line in ${formulae}; do
-                echo "${ident}${line}" | grep "[a-z]" | sed "s_\(.*\)_& | bash=${SCRIPT_DIR}/${SCRIPT_NAME} param1=upgrade param2=--formula param3=& param4=\&\& param5=killall param6=kTerminal terminal=true refresh=true_g"
+                echo "${ident}${line}" | grep "[a-z]" | sed "s_\(.*\)_& | bash=${SCRIPT_DIR}/${SCRIPT_NAME} param1=upgrade param2=--formula param3=& param4=\&\& param5=killall param6=Terminal terminal=true refresh=true_g"
             done
         fi
         if [[ "${count_formulae}" == "0" ]]; then
@@ -87,33 +91,30 @@ if [ $# -eq 0 ]; then
     fi
     echo "Brew Cleanup | bash=${SCRIPT_DIR}/${SCRIPT_NAME} param1=cleanup param2=&& param3=killall param4=Terminal terminal=true refresh=true"
     echo "---"
-    echo "Settings"
-    if [ -f ${ASSETS_DIR}/.notify ]; then
-        echo "-- Disable Notification  | bash=rm param1=-f param2=${ASSETS_DIR}/.notify terminal=false refresh=true"
-    else
-        echo "-- Enable Notification | bash=touch param1=${ASSETS_DIR}/.notify terminal=false refresh=true"
-    fi
-    if [ -f ${ASSETS_DIR}/.darkmode ]; then
-        echo "-- Lightmode | bash=rm param1=-f param2=${ASSETS_DIR}/.darkmode terminal=false refresh=true"
-    else
-        echo "-- Darkmode | bash=touch param1=${ASSETS_DIR}/.darkmode terminal=false refresh=true"
-    fi
-    echo "---"
     echo "Refresh | refresh=true"
 else
     if [ "$#" -eq 3 ] && [ ${1} == 'upgrade' ]; then
-        brew upgrade ${2} ${3}
+        brew upgrade ${2} ${3} 2>&1 | tee ${ASSETS_DIR}/.log
+        cat ${ASSETS_DIR}/.log | sed '1,/Error:/d' | grep -E '^[a-zA-Z0-9_\-]+:' | awk -F ":" '{print $1}' > ${ASSETS_DIR}/.errors
+        sleep 1
+        /usr/bin/open --background xbar://app.xbarapp.com/refreshPlugin?path=${SCRIPT_NAME}
+    fi
+    if [ "$#" -eq 2 ] && [ ${1} == 'reinstall' ]; then
+        brew reinstall ${2} 2>&1
+        cat ${ASSETS_DIR}/.errors | grep -v ${2} > ${ASSETS_DIR}/.errors
+        sleep 1
+        /usr/bin/open --background xbar://app.xbarapp.com/refreshPlugin?path=${SCRIPT_NAME}
+    fi
+    if [ "$#" -eq 2 ] && [ ${1} == 'uninstall' ]; then
+        brew uninstall ${2} 2>&1
+        cat ${ASSETS_DIR}/.errors | grep -v ${2} > ${ASSETS_DIR}/.errors
         sleep 1
         /usr/bin/open --background xbar://app.xbarapp.com/refreshPlugin?path=${SCRIPT_NAME}
     fi
     if [ "$#" -eq 1 ]; then
         if [[ ${1} == 'upgrade-all' ]]; then
-            if [[ "${count_formulae}" != "0" ]]; then
-                brew upgrade --formulae ${formulae}
-            fi
-            if [[ "${count_casks}" != "0" ]]; then
-                brew upgrade --casks ${casks}
-            fi
+            brew upgrade --greedy-auto-updates 2>&1 | tee ${ASSETS_DIR}/.log
+            cat ${ASSETS_DIR}/.log | sed '1,/Error:/d' | grep -E '^[a-zA-Z0-9_\-]+:' | awk -F ":" '{print $1}' > ${ASSETS_DIR}/.errors
             sleep 1
             /usr/bin/open --background xbar://app.xbarapp.com/refreshPlugin?path=${SCRIPT_NAME}
         fi
